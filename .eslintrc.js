9bc08545b9d9ae58beb9dbd4fe3f733b1a3ab066
shiyuejs <313479200@qq.com>
2019-08-28 14:48:31 +0800

    提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
     root: true,
     parserOptions: {
         parser: 'babel-eslint',
-        ecmaVersion: 2019,
-        // ECMAScript modules 模式
-        sourceType: 'module',
-        ecmaFeatures: {
-            // 不允许 return 语句出现在 global 环境下
-            globalReturn: false
-        },
-        // 即使没有 babelrc 配置文件，也使用 babel-eslint 来解析
-        requireConfigFile: false,
-        // 仅允许 import export 语句出现在模块的顶层
-        allowImportExportEverywhere: false
+        // ecmaVersion: 2019,
+        // // ECMAScript modules 模式
+        // sourceType: 'module',
+        // ecmaFeatures: {
+        //     // 不允许 return 语句出现在 global 环境下
+        //     globalReturn: false
+        // },
+        // // 即使没有 babelrc 配置文件，也使用 babel-eslint 来解析
+        // requireConfigFile: false,
+        // // 仅允许 import export 语句出现在模块的顶层
+        // allowImportExportEverywhere: false
     },
     env: {
         browser: true,
